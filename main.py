61bff33cb668aa2ab64bb271d0d587bf82d73dc9
dogeONE-bit <83808696+dogeONE-bit@users.noreply.github.com>
2022-10-23 14:38:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 # coding:utf-8
 import os
 import time
+import sys
+import importlib
+import urllib.parse
 
 from selenium import webdriver#导入库
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+importlib.reload(sys)
+
+def ParseCookiestr(cookie_str):
+    cookielist = []
+    for item in cookie_str.split(';'):
+        cookie={}
+        itemname=item.split('=')[0]
+        iremvalue=item.split('=')[1]
+        cookie['name']=itemname.strip()
+        cookie['value']=urllib.parse.unquote(iremvalue)
+        cookielist.append(cookie)
+    return cookielist
 
 
 if __name__ == "__main__":
     username = os.environ.get("USERNAME", None)
     password = os.environ.get("PASSWORD", None)
+    cookie_str= os.environ.get("COOKIE", None)
 
     #安装google核
     chrome_options = webdriver.ChromeOptions()
@@ -26,6 +42,7 @@ if __name__ == "__main__":
     url_login = 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin'
     url_user = 'https://weixine.ustc.edu.cn/2020/home'
 
+    '''
     # 按钮登陆
     driver.get(url_login)  # 打开浏览器预设网址
     locator1 = (By.XPATH, '//*[@id="username"]')
@@ -35,10 +52,9 @@ if __name__ == "__main__":
     button_login = driver.find_element(By.XPATH,'//*[@id="login"]')
     button_login.click()
 
-    #获取cookie
+    #获取并添加cookie
     cookie_get = driver.get_cookies()
 
-    # 添加cookie
     driver.delete_all_cookies()
     time.sleep(5)
     driver.refresh()
@@ -52,6 +68,36 @@ if __name__ == "__main__":
     driver.refresh()
 
     driver.get(url_user)
+    '''
+
+    #获取并添加cookie(2)
+    cookielist = ParseCookiestr(cookie_str)
+
+    driver.delete_all_cookies()
+    time.sleep(5)
+    driver.refresh()
+    driver.get(url_user)
+
+    for cookie in cookielist:
+        if 'expiry' in cookie:
+            del cookie['expiry']
+    print(cookielist)
+
+    for cookie in cookielist:
+        if cookie["name"] in ['laravel_session', 'XSRF-TOKEN','PHPSESSID']:
+            driver.add_cookie({
+                "domain": 'weixine.ustc.edu.cn',
+                "httpOnly": True,
+                "name": cookie["name"],
+                "path": '/',
+                "secure": False,
+                "value": cookie["value"],
+            })
+
+    driver.refresh()
+    driver.get(url_user)
+
+
 
     #签到
     locator2 = (By.XPATH, '//*[@id="report-submit-btn-a24"]')
